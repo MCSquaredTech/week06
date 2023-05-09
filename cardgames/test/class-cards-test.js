de0c6b40647a9537da4eedcdd5fde12b72eb2875
Mark D Christensen <mark@brooksnet.com>
2023-05-09 16:55:27 -0600

    06 -updates on cards unit test

--- cardgames/test/class-cards-test.js
+++ cardgames/test/class-cards-test.js
@@ -1,29 +1,31 @@
 import { Cards } from '../classes/cards.js';
+import { Card } from '../classes/card.js';
 
 let expect = chai.expect;
-let cards = new Cards("Spades", "King", 10);
-cards.currentCard = cards[0];
+let cards = new Cards()
+cards.addCards("Spades", "King", 10);
+cards.currentCard = cards.cards[0];
 
-describe("CardsClass", function() { 
-    describe("#classCards", function() {
+describe("ClassCards", function() { 
+    describe("#ClassCards", function() {
         it("Expects to return the Card Suit", function () {
             let x = cards.currentCard.suit;
             expect(x).to.equal("Spades");
         });
         it("Expects to return the Card Rank", function () {   
-            let x = card.currentCard.rank;
+            let x = cards.currentCard.rank;
             expect(x).to.equal("King");
         }); 
         it("Expects to return the Card Value", function () { 
-            let x = card.currentCard.value;
+            let x = cards.currentCard.value;
             expect(x).to.equal(10); 
         });
         it("Expects to return the Card Graphic", function () {
-            let x = card.currentCard.graphic;
+            let x = cards.currentCard.graphic;
             expect(x).to.equal('\u2660');
         });
         it("Expects to retrun the Card Ranking Value", function () {
-            let x = card.currentCard.rankValue;
+            let x = cards.currentCard.rankValue;
             expect(x).to.equal(11);
         });
     });
